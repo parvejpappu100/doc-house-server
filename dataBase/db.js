81179a13b3d1b6f93f1ae0ffc800ef626099e169
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2024-09-20 04:36:28 +0600

    connect with mongodb and get client says data

---
+++ dataBase/db.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdnsrak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connect() {
+  await client.connect();
+}
+
+function getClientSaysCollection() {
+  return client.db("doc-house").collection("client-says");
+}
+
+module.exports = {
+  connect,
+  getClientSaysCollection
+};
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./dataBase/db");
 const app = express();
 const cors = require("cors");
 const prot = process.env.PORT || 5000;
@@ -10,6 +11,17 @@ app.use(
     credentials: true,
   })
 );
+app.use(express.json());
+
+// * Connect With mongoDb:
+db.connect();
+
+//* To get clients says collections:
+app.get("/client-says", async (req, res) => {
+  const clientSaysCollections = db.getClientSaysCollection();
+  const result = await clientSaysCollections.find().toArray();
+  res.send(result);
+});
 
 app.get("/", (req, res) => {
   res.send("doc-house server is running.");
