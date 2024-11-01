b5076b8552dd8fd518bd1eaa52b6429ccde829ca
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2024-11-02 00:00:00 +0600

    saved user to db and implement jwt

--- dataBase/db.js
+++ dataBase/db.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdnsrak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -15,11 +14,17 @@ async function connect() {
   await client.connect();
 }
 
+// * COLLECTIONS:-
 function getClientSaysCollection() {
   return client.db("doc-house").collection("client-says");
 }
 
+function usersCollection() {
+  return client.db("doc-house").collection("usersCollection");
+}
+
 module.exports = {
   connect,
-  getClientSaysCollection
+  getClientSaysCollection,
+  usersCollection
 };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("./dataBase/db");
+const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 const prot = process.env.PORT || 5000;
@@ -13,8 +14,54 @@ app.use(
 );
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // * Connect With mongoDb:
 db.connect();
+
+// * secure related:
+app.post("/jwt", (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+
+  res.send({ token });
+});
+
+// * To save users on db:
+app.post("/users", async (req, res) => {
+  const user = req.body;
+
+  const query = { email: user.email };
+  const usersCollection = db.usersCollection();
+  const existingUser = await usersCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: "User already exist" });
+  }
+
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
 
 //* To get clients says collections:
 app.get("/client-says", async (req, res) => {
